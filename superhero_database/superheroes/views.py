ff51b3ed25d90f5f09918e357b33f10e0707fa7e
Eric Lars Lee <ericlarslee@gmail.com>
2021-04-07 14:10:24 -0500

    added a create new superhero feature that will add superhero to the database

--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Superhero
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    all_superheroes = Superhero.objects.all()
+    context = {
+        'all_superheroes': all_superheroes
+    }
+    return render(request, 'superheroes/index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_superpower = request.POST.get('primary_superpower')
+        secondary_superpower = request.POST.get('secondary_superpower')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_superhero_ability=primary_superpower, secondary_superhero_ability=secondary_superpower, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
